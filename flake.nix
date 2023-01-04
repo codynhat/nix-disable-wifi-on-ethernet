1eb25075ef2b0273362dc9f792c3b0c6a371bfbb
Cody Hatfield <cody.hatfield@me.com>
2023-01-04 11:43:45 -0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    packages.${system} = {
+      default = pkgs.writeShellScriptBin "disable-wifi-on-ethernet" ''
+        #!/bin/bash
+        
+        ETHERNET_DEVICE=en7
+        
+        NETWORKSETUP=/usr/sbin/networksetup
+        IFCONFIG=/sbin/ifconfig
+        
+        WiFi=$($NETWORKSETUP -listallhardwareports | grep -A1 "Wi-Fi" | grep Device | cut -f2 -d' ')
+        
+        echo "Run $0"
+        echo "Wifi interface $WiFi"
+        echo "Ethernet interface $ETHERNET_DEVICE"
+        
+        EthernetStatus=$($IFCONFIG | grep -A5 $ETHERNET_DEVICE | grep status | cut -f2 -d' ')
+        echo "Ethernet is currently $EthernetStatus"
+        
+        if [ "$EthernetStatus" != "active" ]; 
+        then 
+            $NETWORKSETUP -setairportpower $WiFi on
+            echo "Wi-Fi set on"
+        else
+            $NETWORKSETUP -setairportpower $WiFi off
+            echo "Wi-Fi set off"
+        fi
+      '';
+    };
+  };
+}
